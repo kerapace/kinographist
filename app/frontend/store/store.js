c70f242d04546137ca4b296088357df0133b31d6
kerapace <jhill01@manhattan.edu>
2021-08-03 17:04:38 -0400

    forgot to remove logger

--- app/frontend/store/store.js
+++ app/frontend/store/store.js
@@ -3,4 +3,4 @@ import logger from "redux-logger";
 import {createStore, applyMiddleware} from "redux";
 import rootReducer from "../reducers/root_reducer";
 
-export default (preloadedState = {}) => createStore(rootReducer,preloadedState,applyMiddleware(thunk,logger));
+export default (preloadedState = {}) => createStore(rootReducer,preloadedState,applyMiddleware(thunk));
